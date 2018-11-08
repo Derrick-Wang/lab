86fe5bf5147434a3c183ac6c838f8dfc88741ecc
DXR <wandereding@gmail.com>
2018-11-08 17:57:33 +0800

    fixmapper

--- src/main/java/com/shu/sfoan/common/mapper/CustomMapper.java
+++ src/main/java/com/shu/sfoan/common/mapper/CustomMapper.java
@@ -1,10 +1,12 @@
 package com.shu.sfoan.common.mapper;
 
+import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
 
 /**
  * Created by dongyang.wang on 2018/11/7 0007.
  */
+@Repository
 public interface CustomMapper <T> extends Mapper<T>, MySqlMapper<T> {
 }
